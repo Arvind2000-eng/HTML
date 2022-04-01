502de06c9f4852c5e067b4c9e6e0965052476ff4
Arvind2000-eng <80538372+Arvind2000-eng@users.noreply.github.com>
2022-04-01 18:20:49 +0530

    6.2 submission

--- HELPERLAND/HelperLand/ViewModels/ChangePassViewModel.cs
+++ HELPERLAND/HelperLand/ViewModels/ChangePassViewModel.cs
@@ -1,11 +1,18 @@
-﻿namespace HelperLand.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HelperLand.ViewModels
 {
     public class ChangePassViewModel
     {
+        [Required(ErrorMessage = "OldPassword Is Required")]
         public string OldPassword { get; set; }
 
+        [Required(ErrorMessage ="Password Is Required")]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\\-_+!@#$%^&*().=<>,?/:;|~{}])\\S{8,24}$", ErrorMessage = "Please Fullfill Required Condition for Password")]
         public string Password { get; set; }
 
+        [Required(ErrorMessage = "Confirm Password Is Required")]
+        [Compare("Password")]
         public string ConfirmPassword { get; set; }
     }
 }
